0869351486d162e035e2f3c0f4fddd689ae25b7f
paulorgnascimento <124225900+paulorgnascimento@users.noreply.github.com>
2023-05-22 01:03:23 -0300

    Versão inicial

---
+++ api-entidade/LogMessageAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace api_entidade
+{
+    public class LogMessageAttribute : Attribute
+    {
+        public string Message { get; }
+
+        public LogMessageAttribute(string message)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ api-entidade/LogMessageFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace api_entidade
+{
+    public class LogMessageFilter : IActionFilter
+    {
+        private readonly ILogger<LogMessageFilter> _logger;
+
+        public LogMessageFilter(ILogger<LogMessageFilter> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var method = context.ActionDescriptor as ControllerActionDescriptor;
+            if (method != null)
+            {
+                var attributes = method.MethodInfo.GetCustomAttributes(typeof(LogMessageAttribute), false);
+                if (attributes.Any())
+                {
+                    var logAttribute = attributes.First() as LogMessageAttribute;
+                    _logger.LogInformation(logAttribute.Message);
+                }
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+    }
+}
